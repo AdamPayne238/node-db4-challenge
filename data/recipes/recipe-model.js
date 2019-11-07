a9556e7027f933e134c35028cca165d4a7998c87
Adam Payne <ajpayne238@gmail.com>
2019-11-07 17:10:23 -0500

    setting up other function models

--- data/recipes/recipe-model.js
+++ data/recipes/recipe-model.js
@@ -1,10 +1,23 @@
 const db = require('../db-config.js');
 
 module.exports = {
-    find,
+    getRecipes,
+    getShoppingList,
+    getInstructions,
 };
 
-function find(){
+// - `getRecipes()`: should return a list of all recipes in the database.
+function getRecipes(){
     return db.select('*')
     .from('recipes');
+}
+
+// - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
+function getShoppingList(recipe_id){
+
+}
+
+// - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
+function getInstructions(recipe_id){
+
 }
--- data/recipes/recipe-router.js
+++ data/recipes/recipe-router.js
@@ -4,9 +4,12 @@ const Recipes = require('./recipe-model.js');
 
 const router = express.Router();
 
+// - `getRecipes()`: should return a list of all recipes in the database.
+// - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
+// - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
 
 router.get('/', (req, res) => {
-    Recipes.find()
+    Recipes.getRecipes()
     .then(recipe => {
       res.json(recipe);
     })
